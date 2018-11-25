44b9ea6ce1cefbb785ec6e8aa2f3124bb9a59963
Namesh Sanjitha <tns.xiii@gmail.com>
2018-11-25 10:37:53 +0530

    commented

--- 016/Problem16.java
+++ 016/Problem16.java
@@ -4,18 +4,18 @@ public class Problem16 {
 
 	public static void main(String args[]) {
 		
+		// calculate number
 		BigInteger num = new BigInteger("2");
-		
 		for(int i = 0; i < 1000-1; i++) {
 			num = num.multiply(BigInteger.valueOf(2));
 		}
-		
 		System.out.println(num);
 		
+		// get sum of digits
 		String numbers = num.toString();
 		int sum = 0;
 		for(int i = 0; i < numbers.length(); i++) {
-			System.out.print(numbers.charAt(i));
+			// System.out.print(numbers.charAt(i));
 			sum += Integer.parseInt(Character.toString(numbers.charAt(i)));
 		}
 		System.out.println("\nAnswer: " + sum);
